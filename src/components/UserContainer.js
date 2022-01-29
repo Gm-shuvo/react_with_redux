ad9809e42670d4d6fc65eb0c56522bf9c20cc37b
Gm-shuvo <gm.shuvo.ru.cse@gmail.com>
2022-01-30 00:44:57 +0600

    installing thunk middleware for async action

--- src/components/UserContainer.js
+++ src/components/UserContainer.js
@@ -1,5 +1,6 @@
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
+
 const UserContainer = () => {
   const {loading, users} = useSelector((state => state.users))
   console.log(loading, users);
